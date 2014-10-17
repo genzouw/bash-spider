98b82cb8afd323bf03be4d814632209e2e3679bb
genzouw <genzouw@gmail.com>
2014-10-17 10:23:32 +0900

    simple url crawler.

---
+++ spider.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -u
+
+# url="www.diy-tool.com"
+url="${1}"
+level="30"
+
+wget --spider -r --level=${level} --no-parent -nv -o wget.log -e robots=off --wait 2 "${url}"
+
+grep -ri 'http://' wget.log | grep -E -v '(\.jpg|\.jpeg|\.gif|\.css|\.js|\.pdf|\.png|\.xls)' | awk '{print $3}'|sort|uniq|sort > site_map.txt
+
+cat $1 |grep -i -E -v '(\.jpg|\.jpeg|\.gif|\.css|\.js|\.pdf|\.png|\.xls|\.ico|\.txt|\.doc|yandexbot|googlebot|YandexDirect|\/upload\/|" 404 |" 301 |" 302 )'|perl -MURI::Escape -lne 'print uri_unescape($_)'|grep yandsearch|awk '{print $1}'|sort|uniq|wc -l
